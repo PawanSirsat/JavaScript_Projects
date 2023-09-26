79b7562daaf5aeb293a4173cfbc32c6d1d199d46
PawanSirsat <p1.sirsat1998@gmail.com>
2023-09-27 03:55:14 +0530

    Add java And python Quizz

--- 08_Coding Quizzer/app.js
+++ 08_Coding Quizzer/app.js
@@ -4,26 +4,57 @@ const form = document.getElementById('quiz-form')
 
 let quizData
 let currentQuestionIndex = 0
-let correct
-let wrong
+let correct, wrong, userlength
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
 
 async function loadQuizData(language, difficulty) {
   try {
     const response = await fetch(`./${language}-quiz.json`)
     quizData = await response.json()
+    //Filter By Difficulty Level
     quizData = quizData[difficulty]
+    //Define length
+    if (userlength > quizData.length) {
+      userlength = quizData.length
+    }
+    console.log('Length ' + userlength)
+
+    shuffleArray(quizData)
+    //Display First Que
     displayQuestion(currentQuestionIndex)
   } catch (error) {
     console.error('Error loading quiz data:', error)
   }
 }
 
-function displayQuestion(questionIndex) {
-  questionContainer.style.display = 'block'
-  const question = quizData[questionIndex]
+function startQuiz() {
+  const language = document.getElementById('language').value
+  const difficulty = document.getElementById('difficulty').value
+  userlength = document.getElementById('quiz-type').value
+
   correct = 0
   wrong = 0
 
+  if (language === 'none' || difficulty === 'none') {
+    alert('Please select a language and difficulty level.')
+    return
+  }
+  loadQuizData(language, difficulty)
+  document.getElementById('quiz-form').style.display = 'none'
+}
+
+function displayQuestion(questionIndex) {
+  questionContainer.style.display = 'block'
+  const question = quizData[questionIndex]
+
+  const shuffledOptions = [...question.options]
+  shuffleArray(shuffledOptions)
   if (!question) {
     endQuiz()
     return
@@ -32,7 +63,7 @@ function displayQuestion(questionIndex) {
           <h2>${question.question}</h2>
           <ul>
            <div id="options-container">
-  ${question.options
+  ${shuffledOptions
     .map(
       (option, index) => `
         <div class="option">
@@ -50,13 +81,42 @@ function displayQuestion(questionIndex) {
   currentQuestionIndex = questionIndex
 }
 
+function checkAnswer(questionIndex) {
+  const selectedAnswerText = document.querySelector(
+    'input[name="answer"]:checked + label'
+  ).textContent
+
+  // Now selectedAnswerText will contain the text of the selected option.
+
+  console.log(selectedAnswerText)
+
+  if (!selectedAnswerText) {
+    alert('Please select an answer.')
+    return
+  }
+  const correctAnswerText = quizData[questionIndex].options.find(
+    (option) => option.correct
+  ).text
+
+  if (selectedAnswerText === correctAnswerText) {
+    correct++
+    alert('Correct answer!')
+  } else {
+    wrong++
+    alert('Wrong answer. Correct Answer : ' + correctAnswerText)
+  }
+  displayNextQuestion()
+}
+
 function displayNextQuestion() {
-  if (currentQuestionIndex < quizData.length - 1) {
+  if (currentQuestionIndex < userlength - 1) {
     displayQuestion(currentQuestionIndex + 1)
   } else {
     document.getElementById('question-container').style.display = 'none'
     result.style.display = 'block'
     const innerHTML = `
+              <h2>Quiz completed!</h2>
+              <h3>Score :  ${correct} / ${correct + wrong} </h3>
               <h3 id="res">Correct : ${correct}</h3>
               <h3 id="res">Wrong : ${wrong}</h3>
               <button type="button" onclick="startAgain()">Start Again</button>
@@ -67,52 +127,15 @@ function displayNextQuestion() {
 }
 
 function startAgain() {
+  currentQuestionIndex = 0
+  form.reset()
   result.style.display = 'none'
   form.style.display = 'block'
-}
-function startQuiz() {
-  const language = document.getElementById('language').value
-  const difficulty = document.getElementById('difficulty').value
-
-  if (language === 'none' || difficulty === 'none') {
-    alert('Please select a language and difficulty level.')
-    return
-  }
-
-  loadQuizData(language, difficulty)
-
-  document.getElementById('quiz-form').style.display = 'none'
-}
-
-function checkAnswer(questionIndex) {
-  const selectedAnswerIndex = document.querySelector(
-    'input[name="answer"]:checked'
-  )
-
-  if (!selectedAnswerIndex) {
-    alert('Please select an answer.')
-    return
-  }
-
-  const correctAnswerIndex = quizData[questionIndex].options.findIndex(
-    (option) => option.correct
-  )
-
-  if (selectedAnswerIndex.value === correctAnswerIndex.toString()) {
-    correct++
-    alert('Correct answer!')
-  } else {
-    wrong++
-    alert('Wrong answer. Try again!')
-  }
-
-  displayNextQuestion()
 }
 
 function endQuiz() {
   const questionContainer = document.getElementById('question-container')
   questionContainer.innerHTML = '<h2>Quiz completed!</h2>'
-  document.getElementById('next-button').style.display = 'none'
 }
 
 document.getElementById('quiz-form').addEventListener('submit', function (e) {
